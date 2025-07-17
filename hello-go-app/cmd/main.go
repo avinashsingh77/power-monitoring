24616089971f0e2a5ebc0ac2d7b41648e834f6b5
Avinash Singh <avsingh@redhat.com>
2025-07-17 16:45:02 +0530

    update imagestream

--- hello-go-app/cmd/main.go
+++ hello-go-app/cmd/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 // Declare the global variable to store the leaking memory
-var memoryLeaker []string
+// var memoryLeaker []string
 
 func main() {
 	for {
@@ -17,37 +17,41 @@ func main() {
 		// Simulate a long-running process
 		time.Sleep(1 * time.Second)
 
-		// Start CPU-intensive goroutine
-		go func() {
-			for {
-				// Perform CPU-intensive calculations
-				for i := 0; i < 1000000; i++ {
-					_ = i * i * i
+		/*
+			// Start CPU-intensive goroutine
+			go func() {
+				for {
+					// Perform CPU-intensive calculations
+					for i := 0; i < 1000000; i++ {
+						_ = i * i * i
+					}
 				}
-			}
-		}()
+			}()
+		*/
 
-		// Memory leak simulation
-		go func() {
-			for {
-				// Allocate memory without releasing it
-				data := make([]string, 100000)
-				for i := range data {
-					data[i] = fmt.Sprintf("leaking memory %d", i)
+		/*
+			// Memory leak simulation
+			go func() {
+				for {
+					// Allocate memory without releasing it
+					data := make([]string, 100000)
+					for i := range data {
+						data[i] = fmt.Sprintf("leaking memory %d", i)
+					}
+					memoryLeaker = append(memoryLeaker, data...)
+
+					// Print current memory stats
+					var m runtime.MemStats
+					runtime.ReadMemStats(&m)
+					fmt.Printf("Allocated memory: %v MB\n", m.Alloc/1024/1024)
+
+					time.Sleep(100 * time.Millisecond)
 				}
-				memoryLeaker = append(memoryLeaker, data...)
-
-				// Print current memory stats
-				var m runtime.MemStats
-				runtime.ReadMemStats(&m)
-				fmt.Printf("Allocated memory: %v MB\n", m.Alloc/1024/1024)
-
-				time.Sleep(100 * time.Millisecond)
-			}
-		}()
+			}()
+		*/
 
 		// Keep main goroutine running
-		select {}
+		// select {}
 
 	}
 }
